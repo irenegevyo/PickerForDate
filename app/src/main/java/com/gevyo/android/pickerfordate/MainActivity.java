5c29c30f403deef5c8cc8e5ca3a20735bf07308f
irenegevyo <irenegevyo@gmail.com>
2019-02-23 00:18:12 +0700

    Initial commit

---
+++ app/src/main/java/com/gevyo/android/pickerfordate/MainActivity.java
@@ -0,0 +1,32 @@
+package com.gevyo.android.pickerfordate;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePicker(View view) {
+        // Merubah DialogFragment objek menjadi DatePickerFragment Objek
+        // Karena dianggap tidak setype...
+        DatePickerFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), getString(R.string.datepicker));
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month + 1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string + "/" + day_string+ "/" + year_string);
+        Toast.makeText(this, "Date: " + dateMessage, Toast.LENGTH_SHORT).show();
+
+    }
+}
+
